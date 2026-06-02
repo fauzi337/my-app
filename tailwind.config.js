e91106f523fb5b37c79b505a6b1cc3cf54512bc4
fauzi337 <abay24ableh@gmail.com>
2026-06-02 21:32:12 +0700

    update ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ import flowbitePlugin from 'flowbite/plugin';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
